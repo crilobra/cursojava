4f2601d0adbd569800b6e17320a33ac1c4c9c15d
crilobra <crilobra@gmail.com>
2017-11-18 15:24:57 -0400

    segundo comit

--- GitVersion/src/gitversion/Figura.java
+++ GitVersion/src/gitversion/Figura.java
@@ -9,14 +9,6 @@ package gitversion;
  *
  * @author ServerB
  */
-public class GitVersion {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application log 
-        System.out.println("hola mundo");
-    }
+public class Figura {
     
 }
